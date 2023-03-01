81e17201b2c26888af254bd0b5864f297870bc23
cshariwal3198 <cshariwal3198@gmail.com>
2023-03-01 10:30:32 +0530

    init commit

--- source/controller/app-controller.ts
+++ source/controller/app-controller.ts
@@ -15,6 +15,8 @@ const { postMethod, deleteMethodCloud, getTodoCloud, putMethod, deleteAllCloud }
 const { createTodoLocal, editTodoLocal, deleteTodoLocal, deleteAllLocal } = localStore()
 const { showEmptyInputError, prepareTask } = view()
 
+// This is master branch, line is just to test.
+
 handlePageRefresh()
 
 function appController() {
