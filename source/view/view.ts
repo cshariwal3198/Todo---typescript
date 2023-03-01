c8db4af56f621c4a321f90da6e5e26808e0bebc0
cshariwal3198 <cshariwal3198@gmail.com>
2023-03-01 11:20:27 +0530

    01/03 - view-local-cloud n remove control

--- source/view/view.ts
+++ source/view/view.ts
@@ -1,10 +1,8 @@
-import { appController, IvalueObjectType } from "../controller/app-controller.js"
+import { IvalueObjectType } from "../utils/todo-item.js"
 
 const taskContainer = document.querySelector(".div-to-display") as HTMLDivElement
 const taskInputBlock = document.querySelector(".form-input") as HTMLInputElement
 const inputError = document.querySelector("#error-div") as HTMLDivElement
-
-let { deleteSingleTask, editSelectedTask, adjustCheckValue } = appController()
 
 function view(){
     return {
@@ -46,13 +44,13 @@ function appendElementToParent(parent : HTMLElement, child : HTMLElement) : void
 function createEditButton(event : string, span : HTMLSpanElement, value : IvalueObjectType) : HTMLElement{
     const editButton = createNewElement("button","Edit") as HTMLButtonElement
     value.isCompleted && (editButton.disabled = true)
-    editButton.addEventListener(event, ()=>editSelectedTask(editButton,span,value.id as number))
+    editButton.addEventListener(event, ()=> console.log("edit"))
     return editButton;
 }
 
 function createDeleteButton(event : string, value : IvalueObjectType) : HTMLElement{
     const deleteButton = createNewElement("button","X") as HTMLButtonElement
-    deleteButton.addEventListener(event, ()=>deleteSingleTask((deleteButton.parentNode as HTMLElement),value))
+    deleteButton.addEventListener(event, ()=> console.log("delete"))
     return deleteButton;
 }
 
@@ -60,7 +58,7 @@ function createCheckBoxElement(event : string, span : HTMLSpanElement, value : I
     const check = createNewElement("input") as HTMLInputElement
     check.type = "checkbox"
     value.isCompleted && (check.checked = true, span.style.textDecoration = "line-through")
-    check.addEventListener(event, () => adjustCheckValue(check, value))
+    check.addEventListener(event, () => console.log("check"))
     return check;
 }
 
